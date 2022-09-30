09231c3ca52ef7d373b575a3115851625a7e90f1
tennin12 <bassembenyhia12@gmail.com>
2022-09-30 17:42:12 +0100

    output fixed

--- 0x00-hello-dart/6-positive_or_negative.dart
+++ 0x00-hello-dart/6-positive_or_negative.dart
@@ -1,10 +1,10 @@
 void main(List<String> arg) {
   int number = int.parse(arg[0]);
   if(number > 0) {
-    print('is positive');
+    print('${number} is positive');
   } else if (number < 0) {
-    print('is negative');
+    print('${number} is negative');
   } else {
-    print('is zero');
+    print('${number} is zero');
   }
 }
