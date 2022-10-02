ed7fbac12ae1f2eba7636fbcc60394033dbb6ea4
tennin12 <bassembenyhia12@gmail.com>
2022-10-02 16:58:31 +0100

    class password output fixed

--- 0x02-dart-classes/2-password.dart
+++ 0x02-dart-classes/2-password.dart
@@ -6,11 +6,11 @@ class Password {
     }
     
     bool isValid() {
-        if (this.password.length <= 8
-             && this.password.length >= 16 
-             && !this.password.contains(RegExp(r'[a-z]'))
-             && !this.password.contains(RegExp(r'[A-Z]'))
-             && !this.password.contains(RegExp(r'[0-9'))
+        if (this.password.length >= 8
+             && this.password.length <= 16
+             && this.password.contains(RegExp(r'[a-z]'))
+             && this.password.contains(RegExp(r'[A-Z]'))
+             && this.password.contains(RegExp(r'[0-9]'))
              ) { return true; }
         return false;
     }
